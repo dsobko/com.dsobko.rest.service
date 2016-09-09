0d0c4184e67d14daf41497798f942ab53b5866a8
dsobko <DFGcvb12>
2016-09-09 15:53:28 +0300

    Add SpringBoot helloWorld

--- service/src/main/java/Application.java
+++ service/src/main/java/Application.java
@@ -1,8 +1,22 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by dsobko on 09/09/2016.
  */
+@RestController
+@EnableAutoConfiguration
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
     }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
