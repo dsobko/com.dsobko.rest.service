1fcfe6170769957e088ba12857c6cff8525ad2cd
dsobko <DFGcvb12>
2016-09-23 15:40:22 +0300

    Move Application class

--- service/src/main/java/com/dsobko/rest/service/Application.java
+++ service/src/main/java/com/dsobko/rest/service/Application.java
@@ -1,19 +1,18 @@
+package com.dsobko.rest.service;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by dsobko on 09/09/2016.
  */
-@RestController
 @EnableAutoConfiguration
+@Configuration
+@ComponentScan
 public class Application {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
